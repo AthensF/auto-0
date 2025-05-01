0e1b658ead9eebe6191286c68c1f058744d764d0
Athens Fitzcheung <athens.fitzcheung@gmail.com>
2025-05-01 12:50:52 -0400

    Phase 1 - uncleaned

--- main.ts
+++ main.ts
@@ -1,23 +1,41 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
-
+import { Editor, MarkdownView, Notice, Plugin, TFile } from "obsidian";
+import { EditorView, ViewPlugin, ViewUpdate, PluginValue } from '@codemirror/view';
 
 export default class MyPlugin extends Plugin {
-
-	async onload() {
-		console.log('woof');
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice no is da best!!');
-		});
+    async onload() {
 		
-	}
+		console.log('woof');
+        
+        
+        // Create the extension separately
+        const typingDetectorExtension = ViewPlugin.fromClass(ExampleViewPlugin);
+        
+        // Register the extension
+		this.registerEditorExtension([typingDetectorExtension]);
+    }
 
-	onunload() {
-		console.log('unloading plugin');
-	}
-
+    onunload() {
+        console.log('unloading plugin');
+    }
 }
 
+class ExampleViewPlugin implements PluginValue {
+	constructor(view: EditorView) {
+		// console.log('Typing detector initialized');
+	}
+	
+	update(update: ViewUpdate) {
+		if (update.docChanged) {
+			console.log('Vet is typing');
+			
+			// Show what changed
+			update.changes.iterChanges((fromA, toA, fromB, toB, text) => {
+				console.log('Text changed:', text.toString());
+			});
+		}
+	}
+	
+	destroy() {
+		console.log('Typing detector destroyed');
+	}
+}
